d0a6cbfbc1dfd2692e92530f2f6041c27567584f
157179179@qq.com <157179179@qq.com>
2025-10-14 15:53:28 +0800

    修改瓦片服务地址

--- ruoyi-ui/public/static/map_load.js
+++ ruoyi-ui/public/static/map_load.js
@@ -4,7 +4,7 @@ let bmapcfg = {
   // 瓦片图的地址，基地址路径（默认名字）
   tiles_dir: '/roadmap',
   // 瓦片图地址-尽量使用127或者localhost-方便断网测试
-  tiles_path: 'http://localhost:3000',
+  tiles_path: 'http://112.98.110.101:3000',
   // 卫星瓦片图的地址-可为空
   tiles_hybrid: '',
   //自定义图层的地址-可为空
