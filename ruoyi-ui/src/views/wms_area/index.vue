39c4867c6c2c03a740686304d7b618afa2f13d77
lwx15846890639 <lwx15846890639@163.com>
2025-10-17 16:39:36 +0800

    区域绘制

--- ruoyi-ui/src/views/wms_area/index.vue
+++ ruoyi-ui/src/views/wms_area/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form
+      :model="queryParams"
+      ref="queryForm"
+      size="small"
+      :inline="true"
+      v-show="showSearch"
+      label-width="68px"
+    >
       <el-form-item label="区域名称" prop="areaName">
         <el-input
           v-model="queryParams.areaName"
@@ -26,8 +33,16 @@
         />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
-        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-search"
+          size="mini"
+          @click="handleQuery"
+          >搜索</el-button
+        >
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery"
+          >重置</el-button
+        >
       </el-form-item>
     </el-form>
 
@@ -40,7 +55,7 @@
           size="mini"
           @click="handleAdd"
           v-hasPermi="['system:area:add']"
-        >新增
+          >新增
         </el-button>
       </el-col>
       <el-col :span="1.5">
@@ -52,7 +67,7 @@
           :disabled="single"
           @click="handleUpdate"
           v-hasPermi="['system:area:edit']"
-        >修改
+          >修改
         </el-button>
       </el-col>
       <el-col :span="1.5">
@@ -64,7 +79,7 @@
           :disabled="multiple"
           @click="handleDelete"
           v-hasPermi="['system:area:remove']"
-        >删除
+          >删除
         </el-button>
       </el-col>
       <el-col :span="1.5">
@@ -75,7 +90,7 @@
           size="mini"
           @click="handleExport"
           v-hasPermi="['system:area:export']"
-        >导出
+          >导出
         </el-button>
       </el-col>
       <el-col :span="1.5">
@@ -86,20 +101,31 @@
           size="mini"
           @click="handleExport"
           v-hasPermi="['system:area:export']"
-        >展示全部
+          >展示全部
         </el-button>
       </el-col>
-      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+      <right-toolbar
+        :showSearch.sync="showSearch"
+        @queryTable="getList"
+      ></right-toolbar>
     </el-row>
 
-    <el-table v-loading="loading" :data="areaList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center"/>
-      <el-table-column label="区域名称" align="center" prop="areaName"/>
-      <el-table-column label="区域类型" align="center" prop="areaType"/>
-      <el-table-column label="风险等级" align="center" prop="areaRiskLevel"/>
-      <el-table-column label="启用状态" align="center" prop="enabled"/>
-      <el-table-column label="备注" align="center" prop="remark"/>
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+    <el-table
+      v-loading="loading"
+      :data="areaList"
+      @selection-change="handleSelectionChange"
+    >
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="区域名称" align="center" prop="areaName" />
+      <el-table-column label="区域类型" align="center" prop="areaType" />
+      <el-table-column label="风险等级" align="center" prop="areaRiskLevel" />
+      <el-table-column label="启用状态" align="center" prop="enabled" />
+      <el-table-column label="备注" align="center" prop="remark" />
+      <el-table-column
+        label="操作"
+        align="center"
+        class-name="small-padding fixed-width"
+      >
         <template slot-scope="scope">
           <el-button
             size="mini"
@@ -107,7 +133,7 @@
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
             v-hasPermi="['system:area:edit']"
-          >修改
+            >修改
           </el-button>
           <el-button
             size="mini"
@@ -115,7 +141,7 @@
             icon="el-icon-no-smoking"
             @click="handleAreaPolygon(scope.row)"
             v-hasPermi="['system:area:edit']"
-          >绘制
+            >绘制
           </el-button>
           <el-button
             size="mini"
@@ -123,14 +149,14 @@
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
             v-hasPermi="['system:area:remove']"
-          >删除
+            >删除
           </el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <pagination
-      v-show="total>0"
+      v-show="total > 0"
       :total="total"
       :page.sync="queryParams.pageNum"
       :limit.sync="queryParams.pageSize"
@@ -141,33 +167,36 @@
     <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="区域名称" prop="areaName">
-          <el-input v-model="form.areaName" placeholder="请输入区域名称"/>
+          <el-input v-model="form.areaName" placeholder="请输入区域名称" />
         </el-form-item>
         <el-form-item label="区域类型" prop="areaFunction">
           <el-select v-model="form.areaType" placeholder="请选择区域类型">
-            <el-option label="办公区域" value="办公区域"/>
-            <el-option label="高危区域" value="高危区域"/>
-            <el-option label="料场区域" value="料场区域"/>
+            <el-option label="办公区域" value="办公区域" />
+            <el-option label="高危区域" value="高危区域" />
+            <el-option label="料场区域" value="料场区域" />
           </el-select>
         </el-form-item>
         <el-form-item label="安全提示" prop="areaSafetyNotice">
-          <el-input v-model="form.areaSafetyNotice" placeholder="请输入安全提示"/>
+          <el-input
+            v-model="form.areaSafetyNotice"
+            placeholder="请输入安全提示"
+          />
         </el-form-item>
         <el-form-item label="风险等级" prop="areaRiskLevel">
           <el-select v-model="form.areaRiskLevel" placeholder="请选择区域类型">
-            <el-option label="低" value="低"/>
-            <el-option label="中" value="中"/>
-            <el-option label="高" value="高"/>
+            <el-option label="低" value="低" />
+            <el-option label="中" value="中" />
+            <el-option label="高" value="高" />
           </el-select>
         </el-form-item>
         <el-form-item label="启用状态" prop="enabled">
           <el-select v-model="form.enabled" placeholder="请选择启用状态">
-            <el-option label="启用" value="启用"/>
-            <el-option label="禁用" value="禁用"/>
+            <el-option label="启用" value="启用" />
+            <el-option label="禁用" value="禁用" />
           </el-select>
         </el-form-item>
         <el-form-item label="备注" prop="remark">
-          <el-input v-model="form.remark" placeholder="请输入备注"/>
+          <el-input v-model="form.remark" placeholder="请输入备注" />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -175,14 +204,26 @@
         <el-button @click="cancel">取 消</el-button>
       </div>
     </el-dialog>
+
+    <mapDetail ref="maps"></mapDetail>
   </div>
 </template>
 
 <script>
-import {listArea, getArea, delArea, addArea, updateArea} from "@/api/system/wms_area"
+import mapDetail from "./childView/map.vue";
+import {
+  listArea,
+  getArea,
+  delArea,
+  addArea,
+  updateArea,
+} from "@/api/system/wms_area";
 
 export default {
   name: "Area",
+  components: {
+    mapDetail,
+  },
   data() {
     return {
       // 遮罩层
@@ -221,31 +262,31 @@ export default {
       // 表单校验
       rules: {
         areaName: [
-          {required: true, message: "区域名称不能为空", trigger: "blur"}
+          { required: true, message: "区域名称不能为空", trigger: "blur" },
         ],
         areaType: [
-          {required: true, message: "区域类型不能为空", trigger: "blur"}
-        ]
-      }
-    }
+          { required: true, message: "区域类型不能为空", trigger: "blur" },
+        ],
+      },
+    };
   },
   created() {
-    this.getList()
+    this.getList();
   },
   methods: {
     /** 查询区域列表 */
     getList() {
-      this.loading = true
-      listArea(this.queryParams).then(response => {
-        this.areaList = response.rows
-        this.total = response.total
-        this.loading = false
-      })
+      this.loading = true;
+      listArea(this.queryParams).then((response) => {
+        this.areaList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
     },
     // 取消按钮
     cancel() {
-      this.open = false
-      this.reset()
+      this.open = false;
+      this.reset();
     },
     // 表单重置
     reset() {
@@ -264,82 +305,90 @@ export default {
         createTime: null,
         updateBy: null,
         updateTime: null,
-        delFlag: null
-      }
-      this.resetForm("form")
+        delFlag: null,
+      };
+      this.resetForm("form");
     },
     /** 搜索按钮操作 */
     handleQuery() {
-      this.queryParams.pageNum = 1
-      this.getList()
+      this.queryParams.pageNum = 1;
+      this.getList();
     },
     /** 重置按钮操作 */
     resetQuery() {
-      this.resetForm("queryForm")
-      this.handleQuery()
+      this.resetForm("queryForm");
+      this.handleQuery();
     },
     // 多选框选中数据
     handleSelectionChange(selection) {
-      this.ids = selection.map(item => item.areaId)
-      this.single = selection.length !== 1
-      this.multiple = !selection.length
+      this.ids = selection.map((item) => item.areaId);
+      this.single = selection.length !== 1;
+      this.multiple = !selection.length;
     },
     /** 新增按钮操作 */
     handleAdd() {
-      this.reset()
-      this.open = true
-      this.title = "添加区域"
+      this.reset();
+      this.open = true;
+      this.title = "添加区域";
     },
     /** 修改按钮操作 */
     handleUpdate(row) {
-      this.reset()
-      const areaId = row.areaId || this.ids
-      getArea(areaId).then(response => {
-        this.form = response.data
-        this.open = true
-        this.title = "修改区域"
-      })
+      this.reset();
+      const areaId = row.areaId || this.ids;
+      getArea(areaId).then((response) => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改区域";
+      });
     },
     /** 提交按钮 */
     submitForm() {
-      this.$refs["form"].validate(valid => {
+      this.$refs["form"].validate((valid) => {
         if (valid) {
           if (this.form.areaId != null) {
-            updateArea(this.form).then(response => {
-              this.$modal.msgSuccess("修改成功")
-              this.open = false
-              this.getList()
-            })
+            updateArea(this.form).then((response) => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
           } else {
-            addArea(this.form).then(response => {
-              this.$modal.msgSuccess("新增成功")
-              this.open = false
-              this.getList()
-            })
+            addArea(this.form).then((response) => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
           }
         }
-      })
+      });
     },
     handleAreaPolygon(row) {
       //TODO 弹出层中绘制多边形及颜色
+      this.$refs.maps.openDia(row);
     },
     /** 删除按钮操作 */
     handleDelete(row) {
-      const areaIds = row.areaId || this.ids
-      this.$modal.confirm('是否确认删除区域编号为"' + areaIds + '"的数据项？').then(function () {
-        return delArea(areaIds)
-      }).then(() => {
-        this.getList()
-        this.$modal.msgSuccess("删除成功")
-      }).catch(() => {
-      })
+      const areaIds = row.areaId || this.ids;
+      this.$modal
+        .confirm('是否确认删除区域编号为"' + areaIds + '"的数据项？')
+        .then(function () {
+          return delArea(areaIds);
+        })
+        .then(() => {
+          this.getList();
+          this.$modal.msgSuccess("删除成功");
+        })
+        .catch(() => {});
     },
     /** 导出按钮操作 */
     handleExport() {
-      this.download('system/area/export', {
-        ...this.queryParams
-      }, `area_${new Date().getTime()}.xlsx`)
-    }
-  }
-}
+      this.download(
+        "system/area/export",
+        {
+          ...this.queryParams,
+        },
+        `area_${new Date().getTime()}.xlsx`
+      );
+    },
+  },
+};
 </script>
